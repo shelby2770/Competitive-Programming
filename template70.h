46d7cae3722c80a28b77ff6a595cb4759567ac11
Adnan Istiaque <108503483+shelby2770@users.noreply.github.com>
2024-03-01 19:08:20 +0600

    Updating...

--- template70.h
+++ template70.h
@@ -5,7 +5,7 @@ using namespace std;
 //Fenwick Tree
 vector<int> BIT;
 
-int get_sum(int idx) {
+int query(int idx) {
     int sum = 0;
     while (idx > 0) {
         sum += BIT[idx];
@@ -19,6 +19,82 @@ void update(int idx, int val) {
         BIT[idx] += val;
         idx += idx & (-idx);
     }
+}
+
+//ST (range query point update)
+vector<int> tree;
+
+void update(int node, int start, int end, int idx, int val) {
+    if (start > idx || end < idx) return;
+    if (start == end) {
+        tree[node] = val; //may need to change
+        return;
+    }
+    int mid = (start + end) / 2;
+    update(node * 2, start, mid, idx, val);
+    update(node * 2 + 1, mid + 1, end, idx, val);
+    tree[node] = tree[node * 2] + tree[node * 2 + 1]; //may need to change
+}
+
+int query(int node, int start, int end, int l, int r) {
+    if (start > r || end < l) return 0; //may need to change
+    if (start >= l && end <= r) return tree[node];
+    int mid = (start + end) / 2;
+    int p1 = query(node * 2, start, mid, l, r);
+    int p2 = query(node * 2 + 1, mid + 1, end, l, r);
+    return p1 + p2; //may need to change
+}
+
+
+//ST (Lazy)
+const int N = 2e5 + 10;
+int tree[4 * N], lazy[4 * N];
+
+void update(int node, int start, int end, int l, int r, int val) {
+    if (lazy[node]) {
+//        tree[node] = (end - start + 1) * lazy[node]; value assignment
+        tree[node] += lazy[node]; //may need to change
+        if (start != end) {
+//            lazy[node * 2] = lazy[node * 2 + 1] = lazy[node]; value assignment
+            lazy[node * 2] += lazy[node]; //may need to change
+            lazy[node * 2 + 1] += lazy[node]; //may need to change
+        }
+        lazy[node] = 0; //may need to change
+    }
+    if (start > end || start > r || end < l) return;
+    if (start >= l && end <= r) {
+//        tree[node] = (end - start + 1) * val; value assignment
+        tree[node] += val; //may need to change
+        if (start != end) {
+//            lazy[node * 2] = lazy[node * 2 + 1] = val; value assignment
+            lazy[node * 2] += val; //may need to change
+            lazy[node * 2 + 1] += val; //may need to change
+        }
+        return;
+    }
+    int mid = (start + end) / 2;
+    update(node * 2, start, mid, l, r, val);
+    update(node * 2 + 1, mid + 1, end, l, r, val);
+    tree[node] = tree[node * 2] + tree[node * 2 + 1];
+}
+
+int query(int node, int start, int end, int l, int r) {
+    if (start > end || start > r || end < l) return 0; //may need to change
+    if (lazy[node]) {
+//        tree[node] = (end - start + 1) * lazy[node]; value assignment
+        tree[node] += lazy[node]; //may need to change
+        if (start != end) {
+//            lazy[node * 2] = lazy[node * 2 + 1] = lazy[node]; value assignment
+            lazy[node * 2] += lazy[node]; //may need to change
+            lazy[node * 2 + 1] += lazy[node]; //may need to change
+        }
+        lazy[node] = 0; //may need to change
+    }
+    if (start >= l && end <= r) return tree[node];
+    int mid = (start + end) / 2;
+    int p1 = query(node * 2, start, mid, l, r);
+    int p2 = query(node * 2 + 1, mid + 1, end, l, r);
+    return p1 + p2;
 }
 
 //LIS
