ee56ebf8ba326ba7a5afd21536421d690d4ef54d
Adnan Istiaque <108503483+shelby2770@users.noreply.github.com>
2024-06-20 20:12:22 +0600

    Update template70.h

--- template70.h
+++ template70.h
@@ -1,11 +1,33 @@
+#pragma GCC target ("avx2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+
 #include<bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 typedef tree<int, null_type, less_equal<>, rb_tree_tag,
         tree_order_statistics_node_update>
-        ordered_set; //o.erase(--(o.lower_bound(v[i])))
+        ordered_set;
+//o.erase(--(o.lower_bound(v[i])))
+//order_of_key: The number of items in a set that are strictly smaller than k
+//find_by_order: It returns an iterator to the ith largest element
 using namespace std;
+
+//need to use for cc_hash_table
+struct hash_pair {
+    template<class T1, class T2>
+    size_t operator()(const pair<T1, T2> &p) const {
+        auto hash1 = hash<T1>{}(p.first);
+        auto hash2 = hash<T2>{}(p.second);
+
+        if (hash1 != hash2) {
+            return hash1 ^ hash2;
+        }
+
+        return hash1;
+    }
+};
 
 //randomized number in a range
 random_device rd;
@@ -39,19 +61,20 @@ unordered_map<long long, int, custom_hash> safe_map;
 vector<int> BIT;
 
 int query(int idx) {
-    int sum = 0;
-    while (idx > 0) {
-        sum += BIT[idx];
-        idx -= idx & (-idx);
-    }
-    return sum;
+    int ret = 0; //may need to change
+    while (idx > 0) ret += BIT[idx],idx -= idx & (-idx);
+    return ret;
 }
 
 void update(int idx, int val) {
-    while (idx < BIT.size()) {
-        BIT[idx] += val;
-        idx += idx & (-idx);
-    }
+    while (idx < N) BIT[idx] += val, idx += idx & (-idx);
+}
+
+//Segment Tree (build)
+int build(int node, int start, int end, vector<int> &v) {
+    if (start == end) return tree[node] = v[start];
+    int mid = (start + end) / 2;
+    return tree[node] = build(node * 2, start, mid, v) + build(node * 2 + 1, mid + 1, end, v);//may need to change
 }
 
 //ST (range query point update)
@@ -131,6 +154,80 @@ int query(int node, int start, int end, int l, int r) {
     return p1 + p2;
 }
 
+//ST (find the number of elements greater than k in a given range l,r)
+const int N = 1e5 + 5;
+vector<int> q(N);
+vector<vector<int>> tree(4 * N);
+
+vector<int> merge(vector<int> &v1, vector<int> &v2) {
+    vector<int> v;
+    int i = 0, j = 0;
+    while (i < v1.size() && j < v2.size()) {
+        if (v1[i] <= v2[j]) {
+            v.push_back(v1[i]);
+            i++;
+        }
+        else {
+            v.push_back(v2[j]);
+            j++;
+        }
+    }
+    for (; i < v1.size(); ++i) v.push_back(v1[i]);
+    for (; j < v2.size(); ++j) v.push_back(v2[j]);
+    return v;
+}
+
+void update(int node, int start, int end) {
+    if (start == end) {
+        tree[node].push_back(q[start]);
+        return;
+    }
+    int mid = (start + end) / 2;
+    update(node * 2, start, mid);
+    update(node * 2 + 1, mid + 1, end);
+    tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
+}
+
+int query(int node, int start, int end, int l, int r, int k) {
+    if (start > r || end < l) return 0;
+    if (start >= l && end <= r)
+        return tree[node].size() - (upper_bound(tree[node].begin(), tree[node].end(), k) - tree[node].begin());
+    int mid = (start + end) / 2;
+    int p1 = query(node * 2, start, mid, l, r, k);
+    int p2 = query(node * 2 + 1, mid + 1, end, l, r, k);
+    return p1 + p2;
+}
+// update(1, 1, N);
+
+//Sparse Table
+int arr[N], st[K + 1][N], lg[N]; //K= log of N
+
+void build() {
+    copy(arr, arr + N, st[0]);
+//    for (int i = 2; i < N; ++i) lg[i] = lg[i / 2] + 1;
+    for (int i = 1; i <= K; ++i)
+        for (int j = 0; j + (1 << i) <= N; ++j) st[i][j] = f(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
+}
+
+//query logn
+int query(int L, int R){
+    int ret = 0; //may need to change
+    for (int i = K; i >= 0; --i) {
+        if ((1 << i) <= R - L + 1) {
+            ret += st[i][L]; //may need to change
+            L += 1 << i;
+        }
+    }
+    return ret;
+}
+
+//query O(1)
+int query(int L, int R){
+//    int i = lg[R - L + 1];
+    int i = lg(R - L + 1);
+    return f(st[i][L], st[i][R - (1 << i) + 1]);
+}
+
 //LIS
 int lis(vector<int> &v, int n) {
     //n^2
@@ -153,6 +250,15 @@ int lis(vector<int> &v, int n) {
 }
 
 //LCS
+s.insert(s.begin(), '$');
+t.insert(t.begin(), '$');
+vector<vector<int>> lcs(n + 1, vector<int>(m + 1));
+for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j <= m; ++j) {
+        if (s[i] == t[j])lcs[i][j] = 1 + lcs[i - 1][j - 1];
+        else lcs[i][j] = max(lcs[i][j - 1], lcs[i - 1][j]);
+    }
+}
 int lcs(string X, string Y, int m, int n,vector<vector<int> >& dp){
     if (m == 0 || n == 0)
         return 0;
@@ -166,7 +272,12 @@ int lcs(string X, string Y, int m, int n,vector<vector<int> >& dp){
 }
 
 //Hashing
-const int N = 1e6, M1 = 1e9 + 7, B1 = 29, M2 = 1e9 + 9, B2 = 31;
+//mt19937_64 rnd(chrono::steady_clock::now().time_since_epoch().count()); for single random number
+random_device rd;
+mt19937 gen(rd());
+int range1 = 31, range2 = 1029;
+uniform_int_distribution<> distr(range1, range2);
+const int N = 1e6, M1 = 1e9 + 7, B1 = distr(gen)/29, M2 = 998244353, B2 = distr(gen)/31;
 vector<int> p1{1}, p2{1};
 
 void pre_calc() {
@@ -195,6 +306,25 @@ struct HashedString {
     }
 };
 
+// Z_function
+vector<int> z_function(string s) {
+    int n = s.size();
+    vector<int> z(n);
+    int l = 0, r = 0;
+    for(int i = 1; i < n; i++) {
+        if(i < r) {
+            z[i] = min(r - i, z[i - l]);
+        }
+        while(i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+            z[i]++;
+        }
+        if(i + z[i] > r) {
+            l = i;
+            r = i + z[i];
+        }
+    }
+    return z;
+}
 
 //operator overloading
 struct triplet {
@@ -225,12 +355,11 @@ struct cmp {
 };
 
 //Sieve of Eratosthenes
-int n;
-vector<bool> is_prime(n+1, true);
+vector<bool> is_prime(N, true);
 is_prime[0] = is_prime[1] = false;
-for (int i = 2; i <= n; i++) {
-    if (is_prime[i] && (long long)i * i <= n) {
-        for (int j = i * i; j <= n; j += i)
+for (int i = 2; i < N; i++) {
+    if (is_prime[i]) {
+        for (int j = i * i; j < N; j += i)
             is_prime[j] = false;
     }
 }
@@ -434,53 +563,66 @@ void phi_1_to_n(int n) {
 }
 
 //LCA - Binary Lifting
-int n, l;
+const int MAX_SIZE = 1 << 10;
+const int MAX_LEVELS = 10; //log of N
+int N, Q;
 vector<vector<int>> adj;
+int depth[MAX_SIZE];
+int parents[MAX_SIZE][MAX_LEVELS];
 
-int timer;
-vector<int> tin, tout;
-vector<vector<int>> up;
-
-void dfs(int v, int p)
-{
-    tin[v] = ++timer;
-    up[v][0] = p;
-    for (int i = 1; i <= l; ++i)
-        up[v][i] = up[up[v][i-1]][i-1];
-
-    for (int u : adj[v]) {
-        if (u != p)
-            dfs(u, v);
+void dfs(int node, int parent) { // dfs to assign depths in the tree
+    parents[node][0] = parent;
+    for (int i: adj[node]) {
+        if (i != parent) {
+            depth[i] = depth[node] + 1;
+            dfs(i, node);
+        }
     }
-
-    tout[v] = ++timer;
 }
 
-bool is_ancestor(int u, int v)
-{
-    return tin[u] <= tin[v] && tout[u] >= tout[v];
-}
-
-int lca(int u, int v)
-{
-    if (is_ancestor(u, v))
+int lca(int u, int v) {
+    if (depth[u] < depth[v]) swap(u, v);
+    for (int i = MAX_LEVELS - 1; i >= 0; --i) {
+        if (depth[u] >= depth[v] + (1 << i)) {
+            u = parents[u][i];
+        }
+    }
+    if (u == v) {
         return u;
-    if (is_ancestor(v, u))
-        return v;
-    for (int i = l; i >= 0; --i) {
-        if (!is_ancestor(up[u][i], v))
-            u = up[u][i];
     }
-    return up[u][0];
+    for (int i = MAX_LEVELS - 1; i >= 0; --i) {
+        if (parents[u][i] != 0 && parents[u][i] != parents[v][i]) {
+            u = parents[u][i];
+            v = parents[v][i];
+        }
+    }
+    return parents[u][0];
 }
 
-void preprocess(int root) {
-    tin.resize(n);
-    tout.resize(n);
-    timer = 0;
-    l = ceil(log2(n));
-    up.assign(n, vector<int>(l + 1));
-    dfs(root, root);
+int main() {
+    cin >> N;
+    adj.resize(N + 1);
+    for (int i = 1; i < N; ++i) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    // preprocessing
+    dfs(1, 0);
+    for (int i = 1; i < MAX_LEVELS; ++i) {
+        for (int j = 1; j <= N; j ++) {
+            if (parents[j][i - 1] != 0) {
+                parents[j][i] = parents[parents[j][i - 1]][i - 1];
+            }
+        }
+    }
+    cin >> Q;
+    while (Q--) {
+        int u, v;
+        cin >> u >> v;
+        cout << "LCA is: " << lca(u, v) << '\n';
+    }
 }
 
 //Permutation
@@ -520,13 +662,12 @@ void union_sets(int a, int b) {
 
 
 //BIGMOD
-long long binpow(long long a, long long b, long long m) {
+int binpow(int a, int b, int m) {
     a %= m;
-    long long res = 1;
+    int res = 1;
     while (b > 0) {
-        if (b & 1)
-            res = res * a % m;
-        a = a * a % m;
+        if (b & 1) res = res * a % m;
+        a = (a * a) % m;
         b >>= 1;
     }
     return res;
@@ -553,6 +694,12 @@ int gcd(int a, int b, int& x, int& y) {
 
 
 //ncr
+int ncr(int n, int r) {
+    int a = fact[n], b = (fact[n - r] * fact[r]) % mod;
+    int inv = binpow(b, mod - 2, mod);
+    return (a*inv)%mod;
+}
+
 int dp[35][35];
 int ncr(int n,int r){
     if(n==r || r==0)return 1;
@@ -627,6 +774,91 @@ for (int i = 1; i <= n; ++i) {
 }
 cout << dp[n][N] << '\n';
 
+//SOS DP
+int n = 20;
+vector<int> a(1 << n);
+
+// keeps track of the sum over subsets
+// with a certain amount of matching bits in the prefix
+vector<vector<int>> dp(1 << n, vector<int>(n));
+
+vector<int> sos(1 << n);
+for (int mask = 0; mask < (1 << n); mask++) {
+	dp[mask][-1] = a[mask];
+	for (int x = 0; x < n; x++) {
+		dp[mask][x] = dp[mask][x - 1];
+		if (mask & (1 << x)) { dp[mask][x] += dp[mask - (1 << x)][x - 1]; }
+	}
+	sos[mask] = dp[mask][n - 1];
+}
+
+
+//Digit DP
+long long dp[20][180][2];
+
+// Stores the digits in x in a vector digit
+void getDigits(long long x, vector<int> &digit) {
+    while (x) {
+        digit.push_back(x % 10);
+        x /= 10;
+    }
+}
+
+// Return sum of digits from 1 to integer in
+// digit vector
+long long digitSum(int idx, int sum, int tight,
+                   vector<int> &digit) {
+    // base case
+    if (idx == -1)
+        return sum;
+
+    // checking if already calculated this state
+    if (dp[idx][sum][tight] != -1 and tight != 1)
+        return dp[idx][sum][tight];
+
+    long long ret = 0;
+
+    // calculating range value
+    int k = (tight) ? digit[idx] : 9;
+
+    for (int i = 0; i <= k; i++) {
+        // calculating newTight value for next state
+        int newTight = (digit[idx] == i) ? tight : 0;
+
+        // fetching answer from next state
+        ret += digitSum(idx - 1, sum + i, newTight, digit);
+    }
+
+    if (!tight)
+        dp[idx][sum][tight] = ret;
+
+    return ret;
+}
+
+// Returns sum of digits in numbers from a to b.
+int rangeDigitSum(int a, int b) {
+    // initializing dp with -1
+    memset(dp, -1, sizeof(dp));
+
+    // storing digits of a-1 in digit vector
+    vector<int> digitA;
+    getDigits(a - 1, digitA);
+
+    // Finding sum of digits from 1 to "a-1" which is passed
+    // as digitA.
+    long long ans1 = digitSum(digitA.size() - 1, 0, 1, digitA);
+
+    // Storing digits of b in digit vector
+    vector<int> digitB;
+    getDigits(b, digitB);
+
+    // Finding sum of digits from 1 to "b" which is passed
+    // as digitB.
+    long long ans2 = digitSum(digitB.size() - 1, 0, 1, digitB);
+
+    return (ans2 - ans1);
+}
+
 //cycle detect (undirected)
 function<bool(int, int)> dfs = [&](int u, int p) -> bool {
     vis[u] = true;
@@ -659,3 +891,54 @@ bool dfs(int v) {
     color[v] = 2;
     return false;
 }
+
+// MOD must be prime for division to work
+template<ll MOD>
+class ModNum {
+    ll val;
+
+public:
+    ModNum (ll val_ = 0) : val(val_ % MOD) {}
+    operator ll() const { return val; }
+
+    ModNum pow (ll p) const {
+        ModNum res = 1;
+        for (ModNum base = val; p > 0; p >>= 1, base *= base)
+            if (p & 1ll)
+                res *= base;
+        return res;
+    }
+
+    ModNum inverse () const {
+        static_assert (MOD >= 2);
+        assert (val != 0);
+
+        return pow(MOD - 2);
+    }
+
+    ModNum operator+(const ModNum& other) const { return (val + other.val) % MOD; }
+    ModNum operator-(const ModNum& other) const { return (val + MOD - other.val) % MOD; }
+    ModNum operator*(const ModNum& other) const { return (val * other.val) % MOD; }
+    ModNum operator/(const ModNum& other) const { return (*this) * other.inverse(); }
+
+    ModNum& operator+=(const ModNum& other) { return *this = *this + other; }
+    ModNum& operator-=(const ModNum& other) { return *this = *this - other; }
+    ModNum& operator*=(const ModNum& other) { return *this = *this * other; }
+    ModNum& operator/=(const ModNum& other) { return *this = *this / other; }
+
+    bool operator==(const ModNum& other) const { return val == other.val; }
+
+
+    friend ModNum operator+(ll a, const ModNum& b) { return ModNum(a) + b; }
+    friend ModNum operator-(ll a, const ModNum& b) { return ModNum(a) - b; }
+    friend ModNum operator*(ll a, const ModNum& b) { return ModNum(a) * b; }
+    friend ModNum operator/(ll a, const ModNum& b) { return ModNum(a) / b; }
+
+    friend bool operator==(ll a, const ModNum& b) { return ModNum(a) == b; }
+
+    friend ostream& operator<<(ostream& os, const ModNum& num) { return os << num.val; }
+    friend istream& operator>>(istream& is, ModNum& num) { return is >> num.val; }
+};
+
+const int P = 1e9 + 7; // Set the Prime
+using mint = ModNum<P>;
